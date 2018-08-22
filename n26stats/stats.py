9359f9ee804481f04251342010c49a8e2210f391
Zack Hsi <zackhsi@gmail.com>
2018-08-21 20:56:26 -0700

    Use a heap and decimals

--- n26stats/stats.py
+++ n26stats/stats.py
@@ -1,34 +1,36 @@
 """
 Stats contains functions for interacting with the global in-memory stats.
+
+Max and min are stored with heaps.
+
+Getting statistics is O(1) because we precompute them on statistic entry and
+statistic expiry.
 """
 from datetime import datetime, timedelta
-from typing import Dict
+from decimal import Decimal
+from heapq import heappop, heappush
+from typing import Dict, List, Tuple
 
 from n26stats.exceptions import StatTooOld
 
 
 class StatsContainer:
     def __init__(self) -> None:
-        self.reset_stats()
+        self.reset()
 
-    def reset_stats(self) -> None:
-        self.avg = 0.0
-        self.count = 0
-        self.max = 0
-        self.min = 0
-        self.sum = 0
+    def reset(self) -> None:
+        self.avg: Decimal = Decimal(0)
+        self.count: Decimal = Decimal(0)
+        self.max: Decimal = Decimal(0)
+        self.min: Decimal = Decimal(0)
+        self.sum: Decimal = Decimal(0)
+        self.min_heap: List[Tuple[Decimal, datetime]] = []
+        self.max_heap: List[Tuple[Decimal, datetime]] = []
 
-    def add(self, amount: int, ts: datetime) -> None:
+    def add(self, amount: Decimal, ts: datetime) -> None:
         now = datetime.now()
         if ts < now - timedelta(minutes=1):
             raise StatTooOld()
-
-        if self.count == 0:
-            self.max = amount
-            self.min = amount
-        else:
-            self.max = max(self.max, amount)
-            self.min = min(self.min, amount)
 
         self.count = self.count + 1
         self.sum = self.sum + amount
@@ -36,15 +38,50 @@ class StatsContainer:
         # Average depends on sum and count.
         self.avg = self.sum / self.count
 
-    def remove(self, value: int) -> None:
-        pass
+        # `heappush` assumes a min heap. We negate the amount in order to
+        # implement a max heap.
+        heappush(self.min_heap, (amount, ts))
+        heappush(self.max_heap, (-amount, ts))
+
+    def remove(self, amount: int) -> None:
+        self.count = self.count - 1
+        self.sum = self.sum - amount
+
+        # Average depends on sum and count.
+        self.avg = self.sum / self.count
+
+    def get_min(self) -> Decimal:
+        if not self.min_heap:
+            return Decimal(0)
+        return self.min_heap[0][0]
+
+    def get_max(self) -> Decimal:
+        if not self.max_heap:
+            return Decimal(0)
+        return self.max_heap[0][0] * -1
+
+    def clean_heaps(self) -> None:
+        for heap_name in ['min_heap', 'max_heap']:
+            heap = getattr(self, heap_name)
+            setattr(
+                self,
+                heap_name,
+                [
+                    (amount, ts) for (amount, ts) in heap
+                    if not self.is_expired(ts)
+                ],
+            )
+
+    def is_expired(self, ts: datetime) -> bool:
+        now = datetime.now()
+        return ts < now - timedelta(minutes=1)
 
     def statistics(self) -> Dict:
         return {
             'avg': self.avg,
             'count': self.count,
-            'max': self.max,
-            'min': self.min,
+            'max': self.get_max(),
+            'min': self.get_min(),
             'sum': self.sum,
         }
 
@@ -61,7 +98,7 @@ def get() -> Dict:
     return stats_container().statistics()
 
 
-def add(amount: int, ts: datetime) -> None:
+def add(amount: Decimal, ts: datetime) -> None:
     stats_container().add(
         amount=amount,
         ts=ts,
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from decimal import Decimal
 
 import pytest
 from pytest_mock import MockFixture
@@ -18,11 +19,11 @@ def reset_stats_container(mocker: MockFixture) -> None:
 
 def test_empty() -> None:
     expected_stats = {
-        'avg': 0,
-        'count': 0,
-        'max': 0,
-        'min': 0,
-        'sum': 0,
+        'avg': Decimal(0),
+        'count': Decimal(0),
+        'max': Decimal(0),
+        'min': Decimal(0),
+        'sum': Decimal(0),
     }
     assert stats.get() == expected_stats
 
@@ -30,39 +31,47 @@ def test_empty() -> None:
 def test_add() -> None:
     now = datetime.now()
     stats.add(
-        amount=10,
+        amount=Decimal(10),
         ts=now,
     )
     expected_stats = {
-        'avg': 10.0,
-        'count': 1,
-        'max': 10,
-        'min': 10,
-        'sum': 10,
+        'avg': Decimal(10),
+        'count': Decimal(1),
+        'max': Decimal(10),
+        'min': Decimal(10),
+        'sum': Decimal(10),
     }
     assert stats.get() == expected_stats
 
     stats.add(
-        amount=-5,
+        amount=Decimal(-5),
         ts=now,
     )
     expected_stats = {
-        'avg': 2.5,
-        'count': 2,
-        'max': 10,
-        'min': -5,
-        'sum': 5,
+        'avg': Decimal(2.5),
+        'count': Decimal(2),
+        'max': Decimal(10),
+        'min': Decimal(-5),
+        'sum': Decimal(5),
     }
     assert stats.get() == expected_stats
 
 
 def test_stat_too_old() -> None:
     stats.add(
-        amount=10,
+        amount=Decimal(10),
         ts=datetime.now() - timedelta(seconds=59)
     )
     with pytest.raises(StatTooOld):
         stats.add(
-            amount=10,
+            amount=Decimal(10),
             ts=datetime.now() - timedelta(seconds=60)
         )
+
+
+def test_remove() -> None:
+    now = datetime.now()
+    stats.add(
+        amount=Decimal(10),
+        ts=now,
+    )
